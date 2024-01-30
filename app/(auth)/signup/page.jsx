acfb060bb47279527cc292e77ecc08b9c4fb737f
Shashank <shashankgupta9248@gmail.com>
2024-01-31 00:05:47 +0530

    :truck: init repo

---
+++ app/(auth)/signup/page.jsx
@@ -0,0 +1,126 @@
+import Link from "next/link";
+import connectToDatabase from "@/lib/connect";
+import User from "@/db/user.schema";
+
+const Signup = async () => {
+  const user_signup = async (formData) => {
+    "use server";
+    await connectToDatabase();
+    const name = formData.get("name");
+    const email = formData.get("email");
+    const password = formData.get("password");
+    console.log(name, email, password);
+    await User.create({ name: name, email: email, password: password });
+    return { message: "User created successfully" };
+  };
+  return (
+    <main className="w-full flex">
+      <div className="relative flex-1 hidden items-center justify-center h-screen bg-gray-900 lg:flex">
+        <div className="relative z-10 w-full max-w-md">
+          logo
+          <div className=" mt-16 space-y-3">
+            <h3 className="text-white text-3xl font-bold">
+              Start growing your business quickly
+            </h3>
+            <p className="text-gray-300">
+              Create an account and get access to all features for 30-days, No
+              credit card required.
+            </p>
+            <div className="flex items-center -space-x-2 overflow-hidden">
+              <img
+                src="https://randomuser.me/api/portraits/women/79.jpg"
+                className="w-10 h-10 rounded-full border-2 border-white"
+              />
+              <img
+                src="https://api.uifaces.co/our-content/donated/xZ4wg2Xj.jpg"
+                className="w-10 h-10 rounded-full border-2 border-white"
+              />
+              <img
+                src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=a72ca28288878f8404a795f39642a46f"
+                className="w-10 h-10 rounded-full border-2 border-white"
+              />
+              <img
+                src="https://randomuser.me/api/portraits/men/86.jpg"
+                className="w-10 h-10 rounded-full border-2 border-white"
+              />
+              <img
+                src="https://images.unsplash.com/photo-1510227272981-87123e259b17?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=3759e09a5b9fbe53088b23c615b6312e"
+                className="w-10 h-10 rounded-full border-2 border-white"
+              />
+              <p className="text-sm text-gray-400 font-medium translate-x-5">
+                Join 5.000+ users
+              </p>
+            </div>
+          </div>
+        </div>
+        <div
+          className="absolute inset-0 my-auto h-[500px]"
+          style={{
+            background:
+              "linear-gradient(152.92deg, rgba(192, 132, 252, 0.2) 4.54%, rgba(232, 121, 249, 0.26) 34.2%, rgba(192, 132, 252, 0.1) 77.55%)",
+            filter: "blur(118px)",
+          }}
+        ></div>
+      </div>
+      <div className="flex-1 flex items-center justify-center h-screen">
+        <div className="w-full max-w-md space-y-8 px-4 bg-white text-gray-600 sm:px-0">
+          <div className="">
+            <img
+              src="https://floatui.com/logo.svg"
+              width={150}
+              className="lg:hidden"
+            />
+            <div className="mt-5 space-y-2">
+              <h3 className="text-gray-800 text-2xl font-bold sm:text-3xl">
+                Sign up
+              </h3>
+              <p className="">
+                Already have an account?{" "}
+                <Link
+                  href={"/login"}
+                  className="font-semibold hover: text-gray-700"
+                >
+                  Log in
+                </Link>
+              </p>
+            </div>
+          </div>
+          <form className="space-y-5" action={user_signup}>
+            <div>
+              <label className="font-medium">Name</label>
+              <input
+                type="text"
+                required
+                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-gray-700 shadow-sm rounded-lg"
+                name="name"
+              />
+            </div>
+            <div>
+              <label className="font-medium">Email</label>
+              <input
+                type="email"
+                required
+                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-gray-700 shadow-sm rounded-lg"
+                name="email"
+              />
+            </div>
+            <div>
+              <label className="font-medium">Password</label>
+              <input
+                type="password"
+                required
+                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-gray-700 shadow-sm rounded-lg"
+                name="password"
+              />
+            </div>
+            <button className="w-full px-4 py-2 text-white font-medium bg-black hover:bg-gray-600 active:bg-gray-700 rounded-lg duration-150">
+              Create account
+            </button>
+          </form>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Signup;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
